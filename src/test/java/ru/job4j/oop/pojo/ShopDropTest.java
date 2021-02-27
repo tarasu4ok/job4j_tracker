98be129da82f035b4ae056c8001f1a1351f08f82
tarasu4ok <taras.kramarenko@gmail.com>
2021-02-28 01:04:52 +0700

    3.1. Массив с пустыми ячейками [#458087]

---
+++ src/test/java/ru/job4j/oop/pojo/ShopDropTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.oop.pojo;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.*;
+
+public class ShopDropTest {
+
+    @Test
+    public void whenDropFirst() {
+        Product[] products = new Product[2];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        Product[] rsl = ShopDrop.leftShift(products, 0);
+        assertThat(rsl[0].getName(), is("Bread"));
+        assertThat(rsl[1], is(nullValue()));
+    }
+
+    @Test
+    public void whenDropLast() {
+        Product[] products = new Product[2];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        Product[] rsl = ShopDrop.leftShift(products, 1);
+        assertThat(rsl[0].getName(), is("Milk"));
+        assertThat(rsl[1], is(nullValue()));
+    }
+
+    @Test
+    public void whenMiddle() {
+        Product[] products = new Product[4];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Cheese", 7);
+        products[3] = new Product("Butter", 2);
+        Product[] rsl = ShopDrop.leftShift(products, 2);
+        assertThat(rsl[0].getName(), is("Milk"));
+        assertThat(rsl[1].getName(), is("Bread"));
+        assertThat(rsl[2].getName(), is("Butter"));
+        assertThat(rsl[3], is(nullValue()));
+    }
+}
