36b3327fefa87fb4d98a7a542e175711d1b25888
tarasu4ok <taras.kramarenko@gmail.com>
2021-02-28 22:47:37 +0700

    2. Модель данных. [#458084]

--- src/main/java/ru/job4j/oop/pojo/College.java
+++ src/main/java/ru/job4j/oop/pojo/College.java
@@ -5,9 +5,9 @@ import java.util.Date;
 public class College {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setFIO("Иванов Иван Иванович");
+        student.setFio("Иванов Иван Иванович");
         student.setGroup(20201);
         student.setEnterDate(new Date());
-        System.out.println("Студент: " + student.getFIO() + ", группа: " + student.getGroup() + ", дата поступления: " + student.getEnterDate());
+        System.out.println("Студент: " + student.getFio() + ", группа: " + student.getGroup() + ", дата поступления: " + student.getEnterDate());
     }
 }
