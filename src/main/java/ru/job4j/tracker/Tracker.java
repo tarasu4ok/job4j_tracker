9816ba363d9258aeb4de14151fee7a40e0c47e9d
tarasu4ok <taras.kramarenko@gmail.com>
2021-03-01 20:58:18 +0700

    7. Метод удаления заявки Tracker.delete [#458089]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,4 +59,14 @@ public class Tracker {
         return true;
     }
 
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        System.arraycopy(items, index + 1, items, index, size - index);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
 }
