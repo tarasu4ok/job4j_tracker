8548c80c35ec37e4b9ea07afed1cd17265079af7
tarasu4ok <taras.kramarenko@gmail.com>
2021-03-02 23:44:46 +0700

    3. Интерфейс Input [#458015]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+        System.out.println("Едем-едем");
+    }
+
+    @Override
+    public void passengers(int count) {
+        System.out.println("В автобус вошло " + count + " пассажиров");
+    }
+
+    @Override
+    public double refuel(int litersCount) {
+        double price = 45.50;
+        return price * litersCount;
+    }
+}
