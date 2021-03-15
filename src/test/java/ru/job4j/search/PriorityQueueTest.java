def7da0c1afc8e5a09a9534078586a75a91d42fe
tarasu4ok <taras.kramarenko@gmail.com>
2021-03-15 18:52:55 +0700

    2. Очередь с приоритетом на LinkedList [#457993]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -19,4 +19,15 @@ public class PriorityQueueTest {
         Task result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
+
+    @Test
+    public void whenEqualPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent1", 1));
+        queue.put(new Task("middle", 3));
+        queue.put(new Task("urgent2", 1));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent1"));
+    }
 }
