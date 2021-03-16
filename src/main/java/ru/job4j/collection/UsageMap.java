caa3607b4d283c69472ac44a8b970162ec9bcd22
tarasu4ok <taras.kramarenko@gmail.com>
2021-03-16 18:19:11 +0700

    0. Map, HashMap [#458000]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<>();
+        map.put("ivanov@yandex.ru", "Ivanov Ivan Ivanovich");
+        map.put("petrov@mail.ru", "Petrov Petr Petrovich");
+        for (Map.Entry<String, String> entry: map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+}
