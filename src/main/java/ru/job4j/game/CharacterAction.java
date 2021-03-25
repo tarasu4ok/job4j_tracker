98736c37c60bcb8542055ed436199c8903c64dd8
tarasu4ok <taras.kramarenko@gmail.com>
2021-03-25 11:56:21 +0700

    Revert "Пошаговая стратегия (экзамен по ООП)"
    This reverts commit e2269819

--- src/main/java/ru/job4j/game/CharacterAction.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.game;
-
-public interface CharacterAction {
-    void firstAction(Character character, Troop activeTroop, Troop enemyTroop);
-
-    void secondAction(Character character, Troop activeTroop, Troop enemyTroop);
-}
