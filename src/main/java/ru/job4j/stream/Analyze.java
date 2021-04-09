7f6c6ad75758d9cb0766719056d48d3da6345230
tarasu4ok <taras.kramarenko@gmail.com>
2021-04-09 12:28:32 +0700

    2. Аттестация [#457842]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,59 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(pupil -> {
+            double avg = Stream.of(pupil)
+                    .flatMap(flatPupil -> flatPupil.getSubjects().stream())
+                    .mapToInt(Subject::getScore)
+                    .average()
+                    .orElse(0D);
+            return new Tuple(pupil.getName(), avg);
+        }
+        )
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, Collectors.averagingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(stringDoubleEntry -> new Tuple(stringDoubleEntry.getKey(), stringDoubleEntry.getValue()))
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(pupil -> {
+            double sum = Stream.of(pupil)
+                    .flatMap(flatPupil -> flatPupil.getSubjects().stream())
+                    .mapToInt(Subject::getScore)
+                    .sum();
+            return new Tuple(pupil.getName(), sum);
+        }
+        ).max(Comparator.comparingDouble(Tuple::getScore))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, Collectors.summingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(stringDoubleEntry -> new Tuple(stringDoubleEntry.getKey(), stringDoubleEntry.getValue()))
+                .max(Comparator.comparingDouble(Tuple::getScore))
+                .orElse(null);
+    }
+}
---
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -0,0 +1,40 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Tuple implements Comparable<Tuple> {
+    private String name;
+    private double score;
+
+    public Tuple(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tuple tuple = (Tuple) o;
+        return Double.compare(tuple.score, score) == 0 &&
+                Objects.equals(name, tuple.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public int compareTo(Tuple o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
