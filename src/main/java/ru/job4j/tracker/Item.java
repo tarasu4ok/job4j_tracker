0656505035b27117b1a03bc5dfc63968488a1d3c
tarasu4ok <taras.kramarenko@gmail.com>
2021-04-15 20:16:11 +0700

    2. Travis CI [#457977]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -52,8 +52,7 @@ public class Item implements Comparable<Item> {
         }
         Item item = (Item) o;
         return id == item.id
-                && Objects.equals(name, item.name)
-                && Objects.equals(created, item.created);
+                && Objects.equals(name, item.name);
     }
 
     @Override
